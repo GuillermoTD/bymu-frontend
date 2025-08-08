28864096922b431eb258c54b607e89c499fabca4
null <null>
2025-08-08 16:52:01 -0400

    se agrega lucide-react y se crea el componente header

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,9 +1,27 @@
+import { Search, ShoppingBag } from 'lucide-react'
 import './HeaderStyles.css'
 
 const Header = ()=>{
     return(
         <div className="Header">
-        Header
+            <div className="Header_Announce paddingGeneral">shopping</div>
+            <div className="Header_Navbar paddingGeneral">
+                <div className="Header_Navbar__Links">
+                    <a href="#">Shop</a>
+                    <a href="#">New Arrivals</a>
+                    <a href="#">Sale</a>
+                </div>
+                <div className="Header_Navbar__Logo text-bold">Bymu</div>
+                  
+                <div className="Header_Navbar__Options">
+                    <Search className='cursor-pointer'/>
+                    <a href="#">Sign Up</a>
+                    <a href="#">Sign In</a>
+                    <div className="badge cursor-pointer">
+                        <ShoppingBag />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
