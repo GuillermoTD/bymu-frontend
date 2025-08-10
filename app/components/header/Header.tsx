fbb256290f316e843885b33b4b9e48c160f4a2d3
GuillermoTD <guillermotd23@gmail.com>
2025-08-10 19:53:28 -0400

    Se termina de crear el header

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -4,7 +4,9 @@ import './HeaderStyles.css'
 const Header = ()=>{
     return(
         <div className="Header">
-            <div className="Header_Announce paddingGeneral">shopping</div>
+            <div className="Header_Announce1 paddingGeneral">
+                Free shipping on orders over $15,000 - Free returning
+            </div>
             <div className="Header_Navbar paddingGeneral">
                 <div className="Header_Navbar__Links">
                     <a href="#">Shop</a>
@@ -14,13 +16,16 @@ const Header = ()=>{
                 <div className="Header_Navbar__Logo text-bold">Bymu</div>
                   
                 <div className="Header_Navbar__Options">
-                    <Search className='cursor-pointer'/>
                     <a href="#">Sign Up</a>
                     <a href="#">Sign In</a>
+                    <Search size={18} className='cursor-pointer'/>
                     <div className="badge cursor-pointer">
-                        <ShoppingBag />
+                    <ShoppingBag size={18} className='cursor-pointer'/>
                     </div>
                 </div>
+            </div>
+            <div className="Header_Announce2 paddingGeneral">
+                Flash Sale Ends In: 22:00:00 
             </div>
         </div>
     )
